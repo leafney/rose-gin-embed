3c4eaa39d30908345e35cfb5df22009b27705db1
leafney <babycoolzx@126.com>
2023-04-06 23:11:37 +0800

    feat: add

---
+++ embed.go
@@ -0,0 +1,33 @@
+package rge
+
+import (
+	"embed"
+	"io/fs"
+	"net/http"
+)
+
+type embedFileSystem struct {
+	http.FileSystem
+}
+
+func (e embedFileSystem) Exists(prefix string, path string) bool {
+	_, err := e.Open(path)
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+func EmbedFolder(fsEmbed embed.FS, targetPath string) ServeFileSystem {
+	fsys, err := fs.Sub(fsEmbed, targetPath)
+	if err != nil {
+		panic(err)
+	}
+	return embedFileSystem{
+		FileSystem: http.FS(fsys),
+	}
+}
+
+func Embed(fsEmbed embed.FS) ServeFileSystem {
+	return EmbedFolder(fsEmbed, ".")
+}
